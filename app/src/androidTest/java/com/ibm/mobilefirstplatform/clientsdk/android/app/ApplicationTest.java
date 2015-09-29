fa072592907cf9ee48185a84bcc28acc0ea4169c
Jim Dickens StephenAlfred <sjimdickens@gmail.com>
2015-09-29 16:58:12 +0530

    Fix test java file references.

--- app/src/androidTest/java/com/ibm/mobilefirstplatform/clientsdk/android/app/ApplicationTest.java
+++ app/src/androidTest/java/com/ibm/mobilefirstplatform/clientsdk/android/app/ApplicationTest.java
@@ -3,10 +3,6 @@ package com.ibm.mobilefirstplatform.clientsdk.android.app;
 import android.app.Application;
 import android.test.ApplicationTestCase;
 
-import com.ibm.mobilefirst.clientsdk.android.push.api.IBMPush;
-import com.ibm.mobilefirst.clientsdk.android.push.api.IBMPushException;
-import com.ibm.mobilefirst.clientsdk.android.push.api.IBMPushResponseListener;
-
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
@@ -14,22 +10,6 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
         super(Application.class);
     }
-
-    public static void main(String[] args){
-        IBMPush push = IBMPush.initializeService();
-
-        push.register("sampleAlias", "sampleConsumerId", new IBMPushResponseListener<String>() {
-
-            @Override
-            public void onSuccess(String o) {
-                System.out.println("Success response in testApp is: " + o);
-            }
-
-            @Override
-            public void onFailure(IBMPushException e) {
-                System.out.println("Failure response in testApp is: " + e.getMessage());
-
-            }
-        });
-    }
 }
+
+
