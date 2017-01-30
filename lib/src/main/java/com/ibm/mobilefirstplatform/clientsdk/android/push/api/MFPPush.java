0355d047131bd0155636800a7783ccf56bb4d2a8
Ananth <agirijak@in.ibm.com>
2017-01-30 23:02:23 +0530

    Changed MFPPUSH_ACTION_NAME to mfpPushActionName

--- lib/src/main/java/com/ibm/mobilefirstplatform/clientsdk/android/push/api/MFPPush.java
+++ lib/src/main/java/com/ibm/mobilefirstplatform/clientsdk/android/push/api/MFPPush.java
@@ -178,7 +178,7 @@ public class MFPPush extends FirebaseInstanceIdService {
     static final String PREFS_MESSAGES_URL_CLIENT_SECRET = "MessagesURLClientSecret";
     static final int INITIALISATION_ERROR = 403;
     static final String PREFS_MESSAGES_OPTIONS = "MessageOptions";
-    static  String MFPPUSH_ACTION_NAME = null;
+    static  String mfpPushActionName = null;
 
     private static MFPPush instance;
     private static Context appContext = null;
@@ -1249,9 +1249,9 @@ public class MFPPush extends FirebaseInstanceIdService {
     private void sendNotificationToListener(MFPInternalPushMessage message) {
         MFPSimplePushNotification simpleNotification = new MFPSimplePushNotification(
                 message);
-        simpleNotification.actionName = MFPPUSH_ACTION_NAME;
+        simpleNotification.actionName = mfpPushActionName;
         notificationListener.onReceive(simpleNotification);
-        MFPPUSH_ACTION_NAME = null;
+        mfpPushActionName = null;
         sendMessageDeliveryStatus(appContext, message.getId(), OPEN);
         relayNotificationSync(message.getKey(), message.getId());
         MFPPush.getInstance().changeStatus(message.getId(), MFPPushNotificationStatus.OPENED);
@@ -1377,7 +1377,7 @@ public class MFPPush extends FirebaseInstanceIdService {
 
     public static void openMainActivityOnNotificationClick(Context ctx) {
         Intent intentToLaunch = ctx.getPackageManager().getLaunchIntentForPackage(ctx.getPackageName());
-        MFPPUSH_ACTION_NAME = getInstance().pushNotificationIntent.getAction();
+        mfpPushActionName = getInstance().pushNotificationIntent.getAction();
 
         if (intentToLaunch != null) {
             intentToLaunch.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT | Intent.FLAG_ACTIVITY_NEW_TASK);
